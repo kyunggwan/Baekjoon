af8bbaa60ae42ee580e9e51909baaa0433501252
kyunggwan <4562sky@naver.com>
2023-02-28 23:50:05 +0900

    [Bronze II] Title: 수 정렬하기, Time: 336 ms, Memory: 22568 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        int[] arr = new int[num];
+        for (int i = 0; i < num; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+
+        for (int i = 0; i < num; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
