c90356f3294fdfd08507afc97d3c55b4f12caef0
kyunggwan <4562sky@naver.com>
2023-03-16 22:54:22 +0900

    Feat: 수동 업로드

---
+++ 백준/Bronze/Num27866.java
@@ -0,0 +1,15 @@
+package Back;
+
+import java.util.Scanner;
+
+public class Num27866 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String word = sc.next();
+        int num = sc.nextInt();
+
+        System.out.println(word.charAt(num-1));
+
+    }
+}
