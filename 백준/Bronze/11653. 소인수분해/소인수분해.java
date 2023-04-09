1258482bf00eebff1e2b7708ba98ea7501b83e7c
kyunggwan <4562sky@naver.com>
2023-04-09 16:29:29 +0900

    [Bronze I] Title: 소인수분해, Time: 128 ms, Memory: 14368 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            while (num % i == 0) {
+                sb.append(i).append('\n');
+                num /= i;
+            }
+        }
+        if (num != 1) {
+            sb.append(num);
+        }
+        System.out.println(sb);
+    }
+}
