a7b0e202c36b0f2183001efe140b659f39f246c4
kyunggwan <4562sky@naver.com>
2024-11-25 23:18:55 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 0.01 ms, Memory: 4.46 MB -BaekjoonHub

---
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// included_len은 배열 included의 길이입니다.
+int solution(int a, int d, bool included[], size_t included_len) {
+    int answer = 0;
+    
+    for(int i = 0 ; i < included_len; i++){
+        if(included[i] == true ){
+            int cal = a + d*(i );
+            answer += cal;
+        }
+    }
+    return answer;
+}
